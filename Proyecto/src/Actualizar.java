27a47e87ed4cf548be1c259fe281a3932b5cce26
sfcg2 <castillo.sofia@correounivalle.edu.co>
2023-11-14 16:18:25 -0500

    ventana actualizar + funcionamiento

--- Proyecto/src/Actualizar.java
+++ Proyecto/src/Actualizar.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Actualizar extends Insertar{
 
     public Actualizar(){}
@@ -8,35 +10,37 @@ public class Actualizar extends Insertar{
 
     public void actualizar(){     
         
-        ArrayList<Candidato> listaC = Insertar.getInscritos();   
-
+        ArrayList<Candidato> listaC = Insertar.getInscritos();
+           
+        Interfazz inz = new Interfazz();
         boolean val4 = false;
-        Candidato cc = new Candidato();
-        System.out.println("\n[Ingrese 0 para salir]\n");
+        Candidato candidato = new Candidato();
+        //String nom = Interfazz.getNom();
+        String cc = Interfazz.getCC();
 
         while(val4 == false){
 
-            System.out.print("Actualizar Candidato -> ");
-            cc.setCedula();
+            //System.out.print("Actualizar Candidato -> ");
+            candidato.setCedula(cc);
 
-            if (cc.getCedula().equals("0")){
+            for(int i = 0; i<listaC.size(); i++){ 
+                
+                if(candidato.getCedula().equalsIgnoreCase(listaC.get(i).getCedula())){
+                    val4 = true;
+                    listaC.remove(i);
+                    inz.inscripciones();                    
+                    JOptionPane.showMessageDialog(inz.wnd, "Candidato encontrado\n ingrese los datos", "ERROR", JOptionPane.INFORMATION_MESSAGE);
+                    inz.dispose();
+                    return;
+                }
+
+            }if(val4 == false){
+
+                JOptionPane.showMessageDialog(inz.wnd, "Candidato NO encontrado,\n vuelva a ingresar la cedula.",
+                "ERROR", JOptionPane.ERROR_MESSAGE);
                 return;
-
-            }else if(cc.getCedula() != "0"){
-
-                for(int i = 0; i<listaC.size(); i++){ 
-                    
-                    if(cc.getCedula().equalsIgnoreCase(listaC.get(i).getCedula())){
-                        val4 = true;
-                        listaC.remove(i);
-                        insertar();
-                    }
-
-                }if(val4 == false){
-                        System.out.println("\nCandidato NO encontrado, vuelva a ingresar la cedula.\n");
-                        System.out.println("[Ingrese 0 para salir]\n");
-                } 
-            }
+            } 
         }
     }
 }
+

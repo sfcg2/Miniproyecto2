42105ce43bcde6fc1ff7961cbcc3ff1effb54535
sfcg2 <castillo.sofia@correounivalle.edu.co>
2023-11-14 10:19:19 -0500

    arreglo errores

--- Proyecto/src/Candidato.java
+++ Proyecto/src/Candidato.java
@@ -10,26 +10,16 @@ public class Candidato extends Ciudadano {
     public String getIdeologia() {
         return ideologia.toUpperCase();
     }
-<<<<<<< HEAD
-    public void setIdeologia() {
-        System.out.print("Ideologia: ");
-        ideologia = scanner.nextLine();
-=======
+
     public void setIdeologia(String idel) {
         this.ideologia = idel;
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
     }
     public String getPartido_P() {
         return partido_P.toUpperCase();
     }
-<<<<<<< HEAD
-    public void setPartido_P() {
-        System.out.print("Partido Politico: ");
-        partido_P = scanner.nextLine();
-=======
+
     public void setPartido_P(String part) {
         this.partido_P = part;
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
     }
     @Override
     public String getCedula() {
@@ -44,18 +34,7 @@ public class Candidato extends Ciudadano {
         return super.getNombre().toUpperCase();
     }
     @Override
-<<<<<<< HEAD
-    public void setCedula() {
-        super.setCedula();
-    }
-    @Override
-    public void setCiudad() {
-        super.setCiudad();
-    }
-    @Override
-    public void setNombre() {
-        super.setNombre();
-=======
+
     public void setCedula(String cc) {
         super.setCedula(cc);
     }
@@ -66,7 +45,6 @@ public class Candidato extends Ciudadano {
     @Override
     public void setNombre(String nom) {
         super.setNombre(nom);
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
     }
     
     @Override
@@ -80,14 +58,8 @@ public class Candidato extends Ciudadano {
         return promesas.toUpperCase();
     }
 
-<<<<<<< HEAD
-    public void setPromesas() {
-        System.out.print("Promesa: ");
-        promesas = scanner.nextLine();
-=======
     public void setPromesas(String prom) {
         this.promesas = prom;
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
     }
 
     public void setsumarVotos() {
--- Proyecto/src/Ciudadano.java
+++ Proyecto/src/Ciudadano.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
 
-<<<<<<< HEAD
-public class Ciudadano {
-=======
-
 public class Ciudadano  {
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
 
     private String nombre;
     private String cedula;
@@ -18,46 +13,26 @@ public class Ciudadano  {
     public String getNombre (){
         return nombre;
     }
-<<<<<<< HEAD
-    public void setNombre() {
-
-        System.out.print("Nombre: ");
-        nombre = scanner.nextLine();
-=======
+    
     public void setNombre(String nom) {
         this.nombre = nom; 
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
     }
 
     public String getCedula() {
         return cedula;
     }
 
-<<<<<<< HEAD
-    public void setCedula() {
-        System.out.print("Cedula: ");
-        cedula = scanner.nextLine();
-=======
     public void setCedula(String cc) {
         this.cedula = cc;
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
     }
 
     public String getCiudad() {
         return ciudad;
     }
 
-<<<<<<< HEAD
-    public void setCiudad() {
-        
-        System.out.print("Ciudad: ");
-        ciudad = scanner.nextLine();
-       
-=======
     public void setCiudad(String cc) {
         
         this.ciudad = cc;
->>>>>>> 4f8d7127c356f427dcf5fb48053d123bc56ccd4a
 
     }
     
